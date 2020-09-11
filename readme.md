fa6d2c7522d8e9404124cfc4b541d4deadd67225
nebelgrau <nebelgrau@gmx.com>
2020-09-11 12:46:31 +0200

    added LED as minimum distance indicator

--- readme.md
+++ readme.md
@@ -2,11 +2,11 @@
 
 Testing STM VL53L0X time-of-flight sensor with  Rust on STM32L031 (Nucleo32 L031K6 board) 
 At the moment it's just reading the distance in single mode, and printing out to serial.
+If the distance is below 100 mm, the built-in LED is turned on.
 
 TO DO:
 * print the distance to an OLED display
 * use a timer to trigger measurements
-* react to the distance, e.g. turn the LED on/off
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn main() -> ! {
 
     //configure the clock
     // let mut rcc = dp.RCC.freeze(Config::hsi16());
-    let mut rcc = dp.RCC.freeze(Config::msi(MSIRange::Range5)); //around 4 MHz, the lowest that actually works here
+    let mut rcc = dp.RCC.freeze(Config::msi(MSIRange::Range6)); //around 4 MHz, the lowest that actually works here
 
     //get the delay provider
     let mut delay = cp.SYST.delay(rcc.clocks);
@@ -35,8 +35,10 @@ fn main() -> ! {
     //delay necessary for the I2C to initiate correctly and start on boot without having to reset the board
     delay.delay_ms(BOOT_DELAY_MS);
 
-    //acquire GPIOA
+    //acquire GPIOA and GPIOB
     let gpioa = dp.GPIOA.split(&mut rcc);
+    
+    let mut gpiob = dp.GPIOB.split(&mut rcc);
 
     //set up I2C
 
@@ -53,18 +55,31 @@ fn main() -> ! {
     let mut serial = dp.USART2.usart(tx_pin, rx_pin, serial::Config::default().baudrate(9600.bps()), &mut rcc).unwrap(); 
    
     let (mut tx, mut _rx) = serial.split();
-
+    
+    // configure built-in LED
+    let mut led = gpiob.pb3.into_push_pull_output();
 
     // initialize Time of Flight sensor
     let mut tof = VL53L0x::new(i2c).unwrap();
 
+    //VL53L0x::start_continuous(&mut tof, 50).unwrap();
+
     loop {
         
         let dist = VL53L0x::read_range_single_millimeters_blocking(&mut tof).unwrap();
+        //let dist = VL53L0x::read_range_continuous_millimeters_blocking(&mut tof).unwrap();
         
+        // turn LED on when distance less than 100 mm
+        if dist < 100 {
+            led.set_high().unwrap();
+        }
+        else {
+            led.set_low().unwrap();
+        }
+
         writeln!(tx, "distance: {} mm\r", dist).unwrap();
 
-        delay.delay_ms(100_u16);
+        delay.delay_ms(50_u16);
 
     }
 
